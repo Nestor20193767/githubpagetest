94b14943b42a64258c87dd245d6b07eff477a358
Nestor Allende <74310266+Nestor20193767@users.noreply.github.com>
2025-01-12 12:44:41 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,20 @@ const toggleMenuButton = document.getElementById('toggle-menu');
 const sidebar = document.querySelector('.sidebar');
 
 toggleMenuButton.addEventListener('click', () => {
+  // Alternar la clase "collapsed" en el menú
   sidebar.classList.toggle('collapsed');
+
   if (sidebar.classList.contains('collapsed')) {
+    // Cuando el menú está colapsado
     sidebar.style.width = '50px';
     toggleMenuButton.textContent = '>';
+    toggleMenuButton.classList.add('outside');
+    toggleMenuButton.style.left = '-20px'; // Mueve el botón hacia afuera del menú
   } else {
+    // Cuando el menú está expandido
     sidebar.style.width = '300px';
     toggleMenuButton.textContent = '<';
+    toggleMenuButton.classList.remove('outside');
+    toggleMenuButton.style.left = ''; // Resetea la posición
   }
 });
